6717eaca43bdd114dab1491601a5609245a196c6
shovon26 <djshovon12@gmail.com>
2023-07-11 15:10:13 +0600

    Handle controlled form with maintaing state of the element

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import './Component/Clock'
-import Clock from "./Component/Clock";
-import ClockList from "./Component/ClockList";
+import Form from './Component/Form'
 function App() {
-    // console.log('App component called');
-    const quantities = [1, 2, 3];
   return (
     <div className="App">
-      <ClockList quantities={quantities}/>
+      <Form />
     </div>
   );
 }
--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,11 +1,70 @@
 import React from "react";
-
+//Controlled vs Uncontrolled Component - 10-07-2023
 export default class Form extends React.Component{
+    state = {
+        title: 'JavaScript',
+        text: 'JavaScript is awesome',
+        library: 'React',
+        isAwesome: true,
+    }
+
+    handleChange = (e) =>{
+        console.log(e.target.type);
+        if(e.target.type === 'text') {
+            this.setState({
+                title: e.target.value,
+            });
+        }
+        else if(e.target.type === 'textarea'){
+            this.setState({
+                text: e.target.value,
+            });
+        }
+        else if(e.target.type === 'select-one'){
+            this.setState({
+                library: e.target.value,
+            });
+        }
+        else if(e.target.type === 'checkbox'){
+            this.setState({
+                isAwesome: e.target.checked,
+            });
+        }
+        else{
+            console.log('No valid block entered');
+        }
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        console.log('Show all: ', this.state);
+    }
+
     render() {
+        let {title, text, library, isAwesome} = this.state;
         return (
             <div>
-                <form>
-                    <input type='text' />
+                <form onSubmit={this.submitHandler}>
+                    <input type='text' placeholder="Enter your name"
+                           value={title}
+                           onChange={this.handleChange}
+                    />
+                    <br/><br/>
+                    <textarea name='text'
+                              value={text}
+                              onChange={this.handleChange}>
+                    </textarea>
+                    <br/><br/>
+                    <select value={library} onChange={this.handleChange}>
+                        <option value='React'>React</option>
+                        <option value='Vue'>Vue</option>
+                        <option value='Angular'>Angular</option>
+                    </select>
+                    <br/> <br/>
+                    <input type='checkbox' checked={isAwesome} onChange={this.handleChange}
+                    />
+                    <br/> <br/>
+                    <input type='submit' value='Submit'/>
                 </form>
             </div>
         );
